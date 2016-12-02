cf16a6c21c5cbcf33f495321a3154a1b50c9416b
rogelio <rogelio@rogelio-ideacentre-AIO-700-24ISH>
2016-12-02 10:54:23 -0500

    garbage

--- deploy.sh
+++
@@ -1,3 +0,0 @@
-echo "Deploying your DAPP..."
-cp -Rpf . ~/.parity/dapps/etyclos.app
-echo "Done! Don´t forget to restart Parity"
--- meBar.js
+++
@@ -1,7 +0,0 @@
-document.write(
-		'"ETH Account: <b>" + myCoinbase + "<br></b> eThyclos Member: <b>" + myIsMember + "</b> Alias: <b>" + myAlias' +
-		'"<br></b> Whisper Account: <b>" + myWhisper' + 
-		'"<br></b>Community: <b>" + myCommunity' +
-		'"</b> Community Name: <b>" + myCommunityName' +
-		'"<br></b> Is Commune: <b>" + isCommune + "</b> Is Bank: <b>" + isBank + "</b>";'
-);
